b0a1e7dee54497d563492b623bcb4a7773f5c040
apralyn eribal <aeribal@wgu.edu>
2023-01-13 01:03:05 -0800

    token set up

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
 
 const saucesCtrl = require("..constrollers/sauces");
 
 const Sauce = require("../models/sauce");
 
-router.get("/", saucesCtrl.getAllSauces);
-router.post("/", saucesCtrl.createSauce);
-router.get("/:id", saucesCtrl.getOneSauce);
-router.put("/:id", saucesCtrl.modifySauce);
-router.delete("/:id", saucesCtrl.deleteSauce);
+router.get("/", auth, saucesCtrl.getAllSauces);
+router.post("/", auth, saucesCtrl.createSauce);
+router.get("/:id", auth, saucesCtrl.getOneSauce);
+router.put("/:id", auth, saucesCtrl.modifySauce);
+router.delete("/:id", auth, saucesCtrl.deleteSauce);
 
 module.exports = router;
